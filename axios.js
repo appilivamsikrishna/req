5330ffe111243ad26f803c52a338da8e0a9ff4cb
Krishna <appilivamsikrishna@gmail.com>
2023-04-10 01:55:51 +0530

    latest

--- axios.js
+++ axios.js
@@ -1,33 +1,27 @@
-const getBtn = document.getElementById('get-btn');
-const postBtn = document.getElementById('post-btn');
+const getBtn = document.getElementById("get-btn");
+const postBtn = document.getElementById("post-btn");
 
 const getData = () => {
-  axios.get('https://reqres.in/api/users').then(response => {
+  axios.get("https://appili.gives/items").then((response) => {
     console.log(response);
   });
 };
 
 const sendData = () => {
   axios
-    .post(
-      'https://reqres.in/api/register',
-      {
-        email: 'eve.holt@reqres.in'
-        // password: 'pistol'
-      },
-      {
-        // headers: {
-        //   'Content-Type': 'application/json'
-        // }
-      }
-    )
-    .then(response => {
+    .put("https://appili.gives/items", {
+      customerName: [1, 2],
+      password: "fuck",
+      username: "helloWorld",
+      id: "1001",
+    })
+    .then((response) => {
       console.log(response);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err, err.response);
     });
 };
 
-getBtn.addEventListener('click', getData);
-postBtn.addEventListener('click', sendData);
+getBtn.addEventListener("click", getData);
+postBtn.addEventListener("click", sendData);
